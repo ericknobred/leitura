80c2c85f71c528d279da496e97c5ded15fea65a1
214860 <erick.damasceno@crefisa.com.br>
2018-06-21 08:47:40 -0300

    Ajuste build e css

--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -4,11 +4,9 @@ import CommentReducer from './CommentReducer'
 import AlertReducer from './AlertReducer'
 import DialogReducer from './DialogReducer'
 import CategoryReducer from './CategoryReducer'
-import { routerReducer } from 'react-router-redux'
 export default combineReducers({
     PostReducer,
     CommentReducer,
     AlertReducer, 
     DialogReducer, 
-    CategoryReducer, 
-    routing: routerReducer})
+    CategoryReducer})
